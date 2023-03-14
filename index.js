b7ce430d2d80d827ca4fd215c43d5cbf41030332
Cvetan Mihaylov <cvetan.mihaylov@gmail.com>
2023-03-14 14:32:17 +0000

    Add basic index.js. Install secp256k1 and crypto deps.

---
+++ index.js
@@ -0,0 +1,15 @@
+const secp256k1 = require('secp256k1')
+const { randomBytes } = require('crypto');
+
+function sign() {
+    return true
+}
+
+function verify() {
+    return true
+}
+
+module.exports = {
+	sign,
+	verify
+}
